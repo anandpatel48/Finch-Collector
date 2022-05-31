f85ab6235cfe45d49bc65b2012774fe0d0fd1372
anandpatel48 <anandrpatel48@gmail.com>
2022-05-31 16:40:28 -0400

    setting up views

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
-from django.urls import pathlib
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.Home.as_view(), name="home"),
+    path('about/', views.About.as_view(), name = "about")
 ]
